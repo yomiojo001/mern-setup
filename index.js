824cfaad6c25dd2dee976cc04aa576976be9b19d
yomiojo001@gmail.com <yomiojo001@gmail.com>
2019-12-16 22:32:34 +0100

    serialize-js212

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ app.get('/api/', (req, res) =>{
 })
 
 app.listen(process.env.PORT || 4000, ()=> console.log('App listening on 4000'));
+
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log("MongoDB database connection established!");
+});
